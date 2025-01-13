42223dfca611534eb938ffaec01586b410dce69f
bwings01 <braedenmwings@gmail.com>
2025-01-13 13:24:15 -0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -3,14 +3,14 @@
 # Student Name: Braeden Wings
 # Assignment Number: Lab 2
 # Due Date: 01/26/2025
-# Purpose: Calculate the amount of whole pizzas needed, salads needed, the total from the cost of whole pizzas and salads, the discount for whole pizzas and salads, the delivery fee, and the total amount due. Print out the the same values in an easy to read format.
+# Purpose: Calculate the amount of whole pizzas needed, salads needed, the total from the cost of whole pizzas and salads, the discount for whole pizzas and salads, the delivery fee, and the total amount due. Print out the same values in an easy-to-read format.
 # I used my knowledge of the python, what I've learned in my IT1114 class and from my Lab, as well as what I've learned from kaggle and other outside resources. Since I used Intellij's pycharm to do my coding, I've also gone and made sure that the code works in IDLE before I submitted.
 
 # math import needed for pizza_salad_order function below
 import math
 # a function to calculate the amount of pizzas needed, the cost of each pizza, the cost of each salad, the total cost of pizzas and salads, the discount, the delivery cost, and the overall total from pizza orders and salad orders
 def pizza_salad_order(pizza_orders, salad_orders):
-    # calculation of pizza orders to show that a whole pizza is 12 slices but each order is 3 slices. the base price of a whole pizza is 15.99. calculation of the total slices needed for the order and the whole pizzas needed by dividing the total slices needed by the slices in a whoe pizza and rounding that to a base number. calculates the total cost of the pizzas by multiplying the amount of whole pizzas needed by the cost of a whole pizza
+    # calculation of pizza orders to show that a whole pizza is 12 slices but each order is 3 slices. the base price of a whole pizza is 15.99. calculation of the total slices needed for the order and the whole pizzas needed by dividing the total slices needed by the slices in a whole pizza and rounding that to a base number. calculates the total cost of the pizzas by multiplying the amount of whole pizzas needed by the cost of a whole pizza
     slices_per_whole_pizza = 12
     slices_per_order = 3
     pizza_cost = 15.99
@@ -48,7 +48,7 @@ def order_up():
     salad_orders = float(input("How many salads would you like to order? "))
     # makes the return values above into variables that will come from the function pizza_salad_order
     whole_pizzas_needed, total_pizza_cost, salad_cost, total_cost, discount, delivery_cost, total_cost_due = pizza_salad_order(pizza_orders, salad_orders)
-    # prints out each of the now defined variables with clear labels beside them to show the transaction total with all the the different parts of the total amount due
+    # prints out each of the now defined variables with clear labels beside them to show the transaction total with all the different parts of the total amount due
     print("Pizzas ordered", whole_pizzas_needed)
     print("Pizza cost $", total_pizza_cost)
     print("Salad cost $", salad_cost)
